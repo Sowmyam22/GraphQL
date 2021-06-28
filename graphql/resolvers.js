472bc82864b2ae2a4f6fe72f07b001b13ff97de8
Sowmyam22 <msowmyadeveloper@gmail.com>
2021-06-28 18:02:14 +0530

    worked on mutation and creating the users in the GraphiQL

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,41 @@
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/user');
+
+// module.exports = {
+//   hello() {
+//     return {
+//       text: 'Hello World!',
+//       views: 123
+//     }
+//   }
+// }
+
+//resolver for the mutation
+
 module.exports = {
-  hello() {
-    return {
-      text: 'Hello World!',
-      views: 123
+  // createUser(args, req) {
+  //   const email = args.userInput.email;
+  // }
+
+  // if not using async await then we should return it when then and catch blocks are used
+
+  createUser: async function ({ userInput }, req) {
+    const existingUser = await User.findOne({ where: { email: userInput.email } });
+
+    if (existingUser) {
+      const error = new Error('User already exists!');
+      throw error;
     }
+
+    const hashedPw = await bcrypt.hash(userInput.password, 12);
+    const user = new User({
+      email: userInput.email,
+      name: userInput.name,
+      password: hashedPw,
+    });
+
+    const createdUser = await user.save();
+    return { _id: createdUser._id, email: createdUser.email }
   }
 }
--- graphql/schema.js
+++ graphql/schema.js
@@ -1,24 +1,66 @@
 const { buildSchema } = require('graphql');
 
-module.exports = buildSchema(`
-  type TestData {
-    text: String!
-    views: Int!
-  }
+// module.exports = buildSchema(`
+//   type TestData {
+//     text: String!
+//     views: Int!
+//   }
 
-  type RootQuery {
-    hello: TestData
-  },
+//   type RootQuery {
+//     hello: TestData
+//   }
 
-  schema {
-    query: RootQuery
-  }
-`);
+//   schema {
+//     query: RootQuery
+//   }
+// `);
 
 
 /** 
+ * Query
  * IT should be the POST request 
  * {
  * "query": "{ hello { text views } }"
  * } 
 **/
+
+// posts: [Post!]! => for adding array pf posts data in the user
+
+module.exports = buildSchema(`
+  type Post {
+    _id: ID!
+    title: String!
+    content: String!
+    imageUrl: String!
+    creator: User!
+    createdAt: String!
+    updatedAt: String!
+  }
+
+  type User {
+    _id: ID!
+    name: String!
+    email: String!
+    password: String
+    status: String!
+  }
+
+  input UserInputData {
+    email: String!
+    name: String!
+    password: String!
+  }
+
+  type RootQuery {
+    hello: String
+  }
+
+  type RootMutation {
+    createUser(userInput: UserInputData): User!
+  } 
+
+  schema {
+    query: RootQuery
+    mutation: RootMutation
+  }
+`)
